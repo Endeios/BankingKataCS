eb6f600899b1885ad8077acd00b3dbb3ca69da76
Bruno Mario Veronesi <endeios@gmail.com>
2020-02-15 14:25:40 +0100

    final assembly and conclusion notes

--- BankKata/Program.cs
+++ BankKata/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BankKata.Lib;
 
 namespace BankKata
 {
@@ -6,7 +7,19 @@ namespace BankKata
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ITransactionsRepo transactionsRepo = new TransactionsRepo();
+            IBankClock clock = new BankClock();
+            IBankConsole console = new BankConsole();
+            ITransactionPrinter transactionPrinter = new BankPrinter(console);
+            var account = new BankAccount(transactionsRepo, clock, transactionPrinter);
+
+            account.Deposit(1500);
+            account.Withdraw(500);
+            account.Deposit(300);
+            account.Deposit(300);
+
+            account.PrintStatement();
         }
     }
 }
+
---
+++ Notes.md
@@ -0,0 +1,12 @@
+# Conclusion of the experiment:
+
+Programming in C# can be pleasent, but it really needs the right tools
+I have been told is much better than java, but is quite the same, not real differnce
+it is true that the source is less verbose, but you still really need an editor
+to be productive in the language. VS without resharper is quite unfriendly as user experience,
+but I understand that is made also to allow the tooling all around.
+VS Code is a little bit more pleasent if you come from the console world, and is much less
+cluttered: I do prefer this small editor for these little programs. 
+
+All in all, if one wants to develop in a sound way, the best is probably to just
+buy the Rider and stick with that.
